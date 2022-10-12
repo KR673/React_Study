c524cb70fd7fcd18b014a26711bc99531a01a109
wys3640 <YS.wangxiaobo@h3c.com>
2022-10-12 15:58:33 +0800

    add UI frame antd

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,7 +1,9 @@
 // Login.js
 import React from 'react'
+import { Button } from 'antd';
+
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>Login <Button type="primary">Button</Button></div>
   )
 }
